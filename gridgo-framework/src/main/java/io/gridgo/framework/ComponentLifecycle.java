5d084ebe42c038daa2e9c59d21bf34336e12b06f
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-03 20:45:29 +0800

    add javadoc

--- gridgo-framework/src/main/java/io/gridgo/framework/ComponentLifecycle.java
+++ gridgo-framework/src/main/java/io/gridgo/framework/ComponentLifecycle.java
@@ -1,11 +1,21 @@
 package io.gridgo.framework;
 
+/**
+ * Represents the lifecycle of a GridGo component. A component
+ * can be a connector, a gateway or a configuration.
+ */
 public interface ComponentLifecycle {
 
+	/**
+	 * Start the component
+	 */
 	public default void start() {
 		
 	}
 	
+	/**
+	 * Stop the component
+	 */
 	public default void stop() {
 		
 	}
