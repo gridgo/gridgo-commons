afcd5981959959cec442b0d84222b90483001565
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2018-11-05 17:07:34 +0800

    added abstract component lifecycle

---
+++ gridgo-framework/src/main/java/io/gridgo/framework/AbstractComponentLifecycle.java
@@ -0,0 +1,8 @@
+package io.gridgo.framework;
+
+import io.gridgo.utils.helper.AbstractStartable;
+import io.gridgo.utils.helper.Loggable;
+
+public abstract class AbstractComponentLifecycle extends AbstractStartable implements ComponentLifecycle, Loggable {
+
+}
