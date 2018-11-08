76f47eb1fff307c18df3ee50affe63b8fd6c1408
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-08 21:43:33 +0800

    add DisruptorExecutionContextStrategy

--- gridgo-framework/src/main/java/io/gridgo/framework/execution/impl/DefaultExecutionStrategy.java
+++ gridgo-framework/src/main/java/io/gridgo/framework/execution/impl/DefaultExecutionStrategy.java
@@ -1,11 +1,17 @@
 package io.gridgo.framework.execution.impl;
 
 import io.gridgo.framework.execution.ExecutionStrategy;
+import io.gridgo.framework.support.context.ExecutionContext;
 import lombok.NonNull;
 
 public class DefaultExecutionStrategy implements ExecutionStrategy {
 
 	public void execute(final @NonNull Runnable runnable) {
 		runnable.run();
+	}
+
+	@Override
+	public void execute(ExecutionContext<?, ?> context) {
+		context.execute();
 	}
 }
