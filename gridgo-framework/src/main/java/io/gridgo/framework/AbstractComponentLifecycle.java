9823755165091539f5f7efac9b830438b8f998ff
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-10 14:41:40 +0800

    merge AbstractStartable and AbstractComponentLifecycle

--- gridgo-framework/src/main/java/io/gridgo/framework/AbstractComponentLifecycle.java
+++ gridgo-framework/src/main/java/io/gridgo/framework/AbstractComponentLifecycle.java
@@ -1,8 +1,46 @@
 package io.gridgo.framework;
 
-import io.gridgo.utils.helper.AbstractStartable;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import io.gridgo.utils.ThreadUtils;
 import io.gridgo.utils.helper.Loggable;
+import io.gridgo.utils.helper.Startable;
 
-public abstract class AbstractComponentLifecycle extends AbstractStartable implements ComponentLifecycle, Loggable {
+public abstract class AbstractComponentLifecycle implements ComponentLifecycle, Loggable, Startable {
 
+	private final AtomicBoolean started = new AtomicBoolean(false);
+	private volatile boolean running = false;
+
+	@Override
+	public final boolean isStarted() {
+		ThreadUtils.busySpin(10, () -> {
+			return this.started.get() ^ this.running;
+		});
+		return this.running;
+	}
+
+	@Override
+	public final void start() {
+		if (!this.isStarted() && started.compareAndSet(false, true)) {
+			this.onStart();
+			this.running = true;
+		}
+	}
+
+	@Override
+	public final void stop() {
+		if (this.isStarted() && started.compareAndSet(true, false)) {
+			try {
+				this.onStop();
+				this.running = false;
+			} catch (Exception e) {
+				this.started.set(true);
+				throw e;
+			}
+		}
+	}
+
+	protected abstract void onStart();
+
+	protected abstract void onStop();
 }
