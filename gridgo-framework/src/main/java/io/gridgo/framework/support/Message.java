aedb2b803250c089d688b3cdf1af3cda8decc64d
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2018-11-15 18:02:40 +0800

    Merge default method from MessageParser to Message as static, delete MessageParser interface

--- gridgo-framework/src/main/java/io/gridgo/framework/support/Message.java
+++ gridgo-framework/src/main/java/io/gridgo/framework/support/Message.java
@@ -1,10 +1,15 @@
 package io.gridgo.framework.support;
 
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.Map;
 import java.util.Optional;
 
+import io.gridgo.bean.BArray;
+import io.gridgo.bean.BElement;
 import io.gridgo.bean.BValue;
 import io.gridgo.framework.support.impl.DefaultMessage;
+import io.gridgo.utils.wrapper.ByteBufferInputStream;
 
 public interface Message {
 
@@ -41,5 +46,47 @@ public interface Message {
 
 	static Message newDefault(BValue routingId, Map<String, Object> misc, Payload payload) {
 		return new DefaultMessage(routingId, misc, payload);
+	}
+
+	static Message parse(byte[] bytes) {
+		return parse(ByteBuffer.wrap(bytes));
+	}
+
+	static Message parse(ByteBuffer buffer) {
+		return parse(new ByteBufferInputStream(buffer));
+	}
+
+	static Message parse(InputStream inputStream) {
+		BElement data = BElement.fromRaw(inputStream);
+		return parse(data);
+	}
+
+	static Message parse(BElement data) {
+		Payload payload = null;
+
+		if (data instanceof BArray && data.asArray().size() == 3) {
+			BArray arr = data.asArray();
+			BElement id = arr.get(0);
+
+			BElement headers = arr.get(1);
+			if (headers.isValue() && headers.asValue().isNull()) {
+				headers = null;
+			}
+
+			BElement body = arr.get(2);
+			if (body.isValue() && body.asValue().isNull()) {
+				body = null;
+			}
+
+			if (id.isValue() && (headers == null || headers.isObject())) {
+				payload = Payload.newDefault(id.asValue(), headers == null ? null : headers.asObject(), body);
+			}
+		}
+
+		if (payload == null) {
+			payload = Payload.newDefault(data);
+		}
+
+		return Message.newDefault(payload);
 	}
 }
