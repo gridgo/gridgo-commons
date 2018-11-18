549f2c346e1ca54450f7021b4e4fa359495d4f73
Dzung Bui <dungba.sg@gmail.com>
2018-11-18 20:58:19 +0800

    add test

--- gridgo-utils/src/test/java/io/gridgo/utils/test/PrimitiveUtilsUnitTest.java
+++ gridgo-utils/src/test/java/io/gridgo/utils/test/PrimitiveUtilsUnitTest.java
@@ -44,7 +44,29 @@ public class PrimitiveUtilsUnitTest {
 		Assert.assertFalse(PrimitiveUtils.getBooleanValueFrom(Boolean.FALSE));
 		Assert.assertTrue(PrimitiveUtils.getBooleanValueFrom(new Object()));
 		Assert.assertFalse(PrimitiveUtils.getBooleanValueFrom(null));
-		Assert.assertTrue(PrimitiveUtils.getBooleanValueFrom(new byte[] {0, 0, 0, 0, 0, 0, 0, 1}));
-		Assert.assertFalse(PrimitiveUtils.getBooleanValueFrom(new byte[] {0, 0, 0, 0, 0, 0, 0, 0}));
+		Assert.assertTrue(PrimitiveUtils.getBooleanValueFrom(new byte[] { 0, 0, 0, 0, 0, 0, 0, 1 }));
+		Assert.assertFalse(PrimitiveUtils.getBooleanValueFrom(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }));
+	}
+
+	@Test
+	public void testGetByte() {
+		Assert.assertEquals(127, PrimitiveUtils.getByteValueFrom(127));
+		Assert.assertEquals(-128, PrimitiveUtils.getByteValueFrom(-128));
+		Assert.assertEquals(97, PrimitiveUtils.getByteValueFrom('a'));
+		Assert.assertEquals(127, PrimitiveUtils.getByteValueFrom("127"));
+		Assert.assertEquals(1, PrimitiveUtils.getByteValueFrom(true));
+		Assert.assertEquals(0, PrimitiveUtils.getByteValueFrom(false));
+		Assert.assertEquals(127, PrimitiveUtils.getByteValueFrom(new byte[] { 127 }));
+	}
+
+	@Test
+	public void testGetShort() {
+		Assert.assertEquals(127, PrimitiveUtils.getShortValueFrom(127));
+		Assert.assertEquals(-128, PrimitiveUtils.getShortValueFrom(-128));
+		Assert.assertEquals(97, PrimitiveUtils.getShortValueFrom('a'));
+		Assert.assertEquals(127, PrimitiveUtils.getShortValueFrom("127"));
+		Assert.assertEquals(1, PrimitiveUtils.getShortValueFrom(true));
+		Assert.assertEquals(0, PrimitiveUtils.getShortValueFrom(false));
+		Assert.assertEquals(257, PrimitiveUtils.getShortValueFrom(new byte[] { 1, 1 }));
 	}
 }
