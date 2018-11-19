e0ba9f42c5aa07136149e483f4cc20cbbd7aec43
Dzung Bui <dungba.sg@gmail.com>
2018-11-19 13:21:24 +0800

    test

--- gridgo-bean/src/test/java/io/gridgo/bean/test/BObjectUnitTest.java
+++ gridgo-bean/src/test/java/io/gridgo/bean/test/BObjectUnitTest.java
@@ -13,13 +13,22 @@ public class BObjectUnitTest {
 	public void testSetAny() {
 		BObject obj = BObject.newDefault() //
 				.set("int", BValue.newDefault(1)) //
+				.setAny("long", 1L) //
+				.setAny("char", 'a') //
 				.setAny("str", "hello") //
+				.setAny("double", 1.11) //
 				.setAny("arr", new int[] { 1, 2, 3 });
 		assertObject(obj);
 		assertObject(obj.deepClone());
 		obj.setAnyIfAbsent("arr", 1);
 		assertObject(obj);
 		Assert.assertTrue(obj.getBoolean("bool", true));
+		obj.setAny("bool", true);
+		Assert.assertTrue(obj.getBoolean("bool", false));
+		Assert.assertEquals(1L, obj.getLong("long", -1));
+		Assert.assertEquals('a', obj.getChar("char", '\0'));
+		Assert.assertEquals(1.11, obj.getDouble("double", -1), 0);
+		Assert.assertEquals(1.11, obj.getFloat("double", -1), 0.001);
 	}
 
 	private void assertObject(BObject obj) {
