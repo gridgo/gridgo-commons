249e260a050108e650ac6e0ad512e986198418cb
Dzung Bui <dungba.sg@gmail.com>
2018-11-19 13:14:30 +0800

    add tests

--- gridgo-bean/src/test/java/io/gridgo/bean/test/BObjectUnitTest.java
+++ gridgo-bean/src/test/java/io/gridgo/bean/test/BObjectUnitTest.java
@@ -3,6 +3,7 @@ package io.gridgo.bean.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import io.gridgo.bean.BArray;
 import io.gridgo.bean.BObject;
 import io.gridgo.bean.BValue;
 
@@ -14,10 +15,18 @@ public class BObjectUnitTest {
 				.set("int", BValue.newDefault(1)) //
 				.setAny("str", "hello") //
 				.setAny("arr", new int[] { 1, 2, 3 });
-		Assert.assertEquals(1, obj.getInteger("int"));
-		Assert.assertEquals("hello", obj.getString("str"));
+		assertObject(obj);
+		assertObject(obj.deepClone());
+		obj.setAnyIfAbsent("arr", 1);
+		assertObject(obj);
+		Assert.assertTrue(obj.getBoolean("bool", true));
+	}
+
+	private void assertObject(BObject obj) {
+		Assert.assertEquals(1, obj.getInteger("int", -1));
+		Assert.assertEquals("hello", obj.getString("str", null));
 		Assert.assertArrayEquals(new Integer[] { 1, 2, 3 }, //
-				obj.getArray("arr").stream() //
+				obj.getArray("arr", BArray.newDefault()).stream() //
 						.map(e -> e.asValue().getData()) //
 						.toArray(size -> new Integer[size]));
 	}
