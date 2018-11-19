04f43e660e352e158ca0b833c822747a10742607
Dzung Bui <dungba.sg@gmail.com>
2018-11-19 22:47:32 +0800

    add tests

---
+++ gridgo-bean/src/test/java/io/gridgo/bean/test/BValueUnitTest.java
@@ -0,0 +1,22 @@
+package io.gridgo.bean.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.gridgo.bean.BValue;
+
+public class BValueUnitTest {
+
+	@Test
+	public void testEncodeDecode() {
+		var val = BValue.newDefault(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
+		val.encodeHex();
+		Assert.assertEquals("0x01020408102040", val.getData());
+		val.decodeHex();
+		Assert.assertArrayEquals(new byte[] { 1, 2, 4, 8, 16, 32, 64 }, (byte[]) val.getData());
+		val.encodeBase64();
+		Assert.assertEquals("AQIECBAgQA==", val.getData());
+		val.decodeBase64();
+		Assert.assertArrayEquals(new byte[] { 1, 2, 4, 8, 16, 32, 64 }, (byte[]) val.getData());
+	}
+}
