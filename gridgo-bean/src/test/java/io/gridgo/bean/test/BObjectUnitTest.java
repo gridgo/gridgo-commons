2190490bc4bbaa4878d16c1db09d808f13486169
Dzung Bui <dungba.sg@gmail.com>
2018-11-19 13:04:29 +0800

    fix unit test

--- gridgo-bean/src/test/java/io/gridgo/bean/test/BObjectUnitTest.java
+++ gridgo-bean/src/test/java/io/gridgo/bean/test/BObjectUnitTest.java
@@ -4,17 +4,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import io.gridgo.bean.BObject;
+import io.gridgo.bean.BValue;
 
 public class BObjectUnitTest {
 
 	@Test
 	public void testSetAny() {
 		BObject obj = BObject.newDefault() //
-				.setAny("int", 1) //
+				.set("int", BValue.newDefault(1)) //
 				.setAny("str", "hello") //
 				.setAny("arr", new int[] { 1, 2, 3 });
 		Assert.assertEquals(1, obj.getInteger("int"));
 		Assert.assertEquals("hello", obj.getString("str"));
-		Assert.assertArrayEquals(new Integer[] { 1, 2, 3 }, obj.getArray("arr").toArray(new Integer[0]));
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3 }, //
+				obj.getArray("arr").stream() //
+						.map(e -> e.asValue().getData()) //
+						.toArray(size -> new Integer[size]));
 	}
 }
