85861b184f134233e48b05bd9d3f9529f56e9e03
Dzung Bui <dungba.sg@gmail.com>
2018-11-20 13:37:38 +0800

    add tests

--- gridgo-utils/src/test/java/io/gridgo/utils/test/ByteArrayUnitTest.java
+++ gridgo-utils/src/test/java/io/gridgo/utils/test/ByteArrayUnitTest.java
@@ -9,13 +9,16 @@ public class ByteArrayUnitTest {
 
 	@Test
 	public void testSimple() {
-		var byteArray = ByteArray.newInstance(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
-		Assert.assertEquals(-1603446134, byteArray.hashCode());
-		byteArray = ByteArray.newInstanceWithJavaSafeHashCodeCalculator(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
-		Assert.assertEquals(1046614007, byteArray.hashCode());
-		byteArray = ByteArray.newInstanceWithJavaUnsafeHashCodeCalculator(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
-		Assert.assertEquals(1046614007, byteArray.hashCode());
-		byteArray = ByteArray.newInstanceWithJNIHashCodeCalculator(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
-		Assert.assertEquals(1046614007, byteArray.hashCode());
+		var byteArray1 = ByteArray.newInstance(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
+		Assert.assertEquals(-1603446134, byteArray1.hashCode());
+		var byteArray2 = ByteArray.newInstanceWithJavaSafeHashCodeCalculator(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
+		Assert.assertEquals(1046614007, byteArray2.hashCode());
+		var byteArray3 = ByteArray.newInstanceWithJavaUnsafeHashCodeCalculator(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
+		Assert.assertEquals(1046614007, byteArray3.hashCode());
+		var byteArray4 = ByteArray.newInstanceWithJNIHashCodeCalculator(new byte[] { 1, 2, 4, 8, 16, 32, 64 });
+		Assert.assertEquals(1046614007, byteArray4.hashCode());
+		Assert.assertEquals(byteArray1, byteArray2);
+		Assert.assertEquals(byteArray1, byteArray3);
+		Assert.assertEquals(byteArray1, byteArray4);
 	}
 }
