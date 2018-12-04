a8a8659d3c4a910862a970b8a6cb68b4edf16e8b
griever <griever@10.100.203.62>
2018-12-04 15:06:18 +0700

    refactor

--- gridgo-framework/src/main/java/io/gridgo/framework/support/Registry.java
+++ gridgo-framework/src/main/java/io/gridgo/framework/support/Registry.java
@@ -1,8 +1,12 @@
 package io.gridgo.framework.support;
 
+import java.util.regex.Pattern;
+
 import io.gridgo.framework.support.exceptions.BeanNotFoundException;
 
 public interface Registry {
+
+	public static final Pattern REGISTRY_SUB_PATTERN = Pattern.compile("\\$\\{([^\\{]*)\\}");
 
 	public Object lookup(String name);
 
@@ -27,5 +31,17 @@ public interface Registry {
 		if (answer == null)
 			throw new BeanNotFoundException("Bean " + name + " cannot be found using" + this.getClass().getName());
 		return answer;
+	}
+
+	public default String substituteRegistries(String endpoint) {
+		if (endpoint.indexOf('$') == -1)
+			return endpoint;
+		var matcher = REGISTRY_SUB_PATTERN.matcher(endpoint);
+		if (!matcher.find())
+			return endpoint;
+		return matcher.replaceAll(result -> {
+			var obj = lookup(result.group(1));
+			return obj != null ? obj.toString() : "";
+		});
 	}
 }
