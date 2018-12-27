66856bcd8046baad2f15a1e0581e29a8f7f23724
Anh Dung Bui <griever@192.168.1.34>
2018-12-27 23:46:31 +0700

    refactor

--- gridgo-utils/src/main/java/io/gridgo/utils/PrimitiveUtils.java
+++ gridgo-utils/src/main/java/io/gridgo/utils/PrimitiveUtils.java
@@ -2,6 +2,9 @@ package io.gridgo.utils;
 
 import java.math.BigDecimal;
 
+import io.gridgo.utils.exception.UnsupportedTypeException;
+import lombok.NonNull;
+
 public class PrimitiveUtils {
 
     private PrimitiveUtils() {
@@ -31,159 +34,135 @@ public class PrimitiveUtils {
     }
 
     @SuppressWarnings("unchecked")
-    public static final <T> T getValueFrom(Class<T> resultType, Object obj) {
-        if (resultType != null) {
-            if (obj == null) {
-                return null;
-            } else if (resultType.isAssignableFrom(obj.getClass())) {
-                return (T) obj;
-            } else if (resultType == String.class) {
-                return (T) getStringValueFrom(obj);
-            } else if (resultType == BigDecimal.class) {
-                if (obj instanceof Number) {
-                    if (obj instanceof BigDecimal) {
-                        return (T) (BigDecimal) obj;
-                    }
-                    return (T) BigDecimal.valueOf(((Number) obj).doubleValue());
+    public static final <T> T getValueFrom(@NonNull Class<T> resultType, Object obj) {
+        if (obj == null)
+            return null;
+        if (resultType.isAssignableFrom(obj.getClass()))
+            return (T) obj;
+        if (resultType == String.class)
+            return (T) getStringValueFrom(obj);
+        if (resultType == BigDecimal.class) {
+            if (obj instanceof Number) {
+                if (obj instanceof BigDecimal) {
+                    return (T) (BigDecimal) obj;
                 }
-                return (T) new BigDecimal(getStringValueFrom(obj));
-            } else if (resultType == Integer.TYPE || resultType == Integer.class) {
-                return (T) Integer.valueOf(getIntegerValueFrom(obj));
-            } else if (resultType == Float.TYPE || resultType == Float.class) {
-                return (T) Float.valueOf(getFloatValueFrom(obj));
-            } else if (resultType == Long.TYPE || resultType == Long.class) {
-                return (T) Long.valueOf(getLongValueFrom(obj));
-            } else if (resultType == Double.TYPE || resultType == Double.class) {
-                return (T) Double.valueOf(getDoubleValueFrom(obj));
-            } else if (resultType == Short.TYPE || resultType == Short.class) {
-                return (T) Short.valueOf(getShortValueFrom(obj));
-            } else if (resultType == Byte.TYPE || resultType == Byte.class) {
-                return (T) Byte.valueOf(getByteValueFrom(obj));
-            } else if (resultType == Character.TYPE || resultType == Character.class) {
-                return (T) Character.valueOf(getCharValueFrom(obj));
-            } else if (resultType == Boolean.TYPE || resultType == Boolean.class) {
-                return (T) Boolean.valueOf(getBooleanValueFrom(obj));
-            } else {
-                throw new RuntimeException("type doesn't supported");
+                return (T) BigDecimal.valueOf(((Number) obj).doubleValue());
             }
+            return (T) new BigDecimal(getStringValueFrom(obj));
         }
-        throw new RuntimeException("result type must be specificed");
+        if (resultType == Integer.TYPE || resultType == Integer.class)
+            return (T) Integer.valueOf(getIntegerValueFrom(obj));
+        if (resultType == Float.TYPE || resultType == Float.class)
+            return (T) Float.valueOf(getFloatValueFrom(obj));
+        if (resultType == Long.TYPE || resultType == Long.class)
+            return (T) Long.valueOf(getLongValueFrom(obj));
+        if (resultType == Double.TYPE || resultType == Double.class)
+            return (T) Double.valueOf(getDoubleValueFrom(obj));
+        if (resultType == Short.TYPE || resultType == Short.class)
+            return (T) Short.valueOf(getShortValueFrom(obj));
+        if (resultType == Byte.TYPE || resultType == Byte.class)
+            return (T) Byte.valueOf(getByteValueFrom(obj));
+        if (resultType == Character.TYPE || resultType == Character.class)
+            return (T) Character.valueOf(getCharValueFrom(obj));
+        if (resultType == Boolean.TYPE || resultType == Boolean.class)
+            return (T) Boolean.valueOf(getBooleanValueFrom(obj));
+        throw new UnsupportedTypeException("Unsupported type: " + resultType.getName());
     }
 
     public static final String getStringValueFrom(Object obj) {
-        if (obj == null) {
+        if (obj == null)
             return null;
-        } else if (obj instanceof String) {
+        if (obj instanceof String)
             return (String) obj;
-        } else if (obj instanceof byte[]) {
-            // return ByteArrayUtils.toHex((byte[]) obj, "0x");
+        if (obj instanceof byte[])
             return new String((byte[]) obj);
-        } else if (isPrimitive(obj.getClass())) {
+        if (isPrimitive(obj.getClass()))
             return String.valueOf(obj);
-        }
         return obj.toString();
     }
 
-    public static final int getIntegerValueFrom(Object obj) {
-        if (obj != null) {
-            if (obj instanceof Number) {
-                return ((Number) obj).intValue();
-            } else if (obj instanceof Character) {
-                return ((Character) obj).charValue();
-            } else if (obj instanceof String) {
-                return Integer.valueOf((String) obj);
-            } else if (obj instanceof Boolean) {
-                return (Boolean) obj ? 1 : 0;
-            } else if (obj instanceof byte[]) {
-                return ByteArrayUtils.bytesToPrimitive(Integer.class, (byte[]) obj);
-            }
-        }
+    public static final int getIntegerValueFrom(@NonNull Object obj) {
+        if (obj instanceof Number)
+            return ((Number) obj).intValue();
+        if (obj instanceof Character)
+            return ((Character) obj).charValue();
+        if (obj instanceof String)
+            return Integer.valueOf((String) obj);
+        if (obj instanceof Boolean)
+            return (Boolean) obj ? 1 : 0;
+        if (obj instanceof byte[])
+            return ByteArrayUtils.bytesToPrimitive(Integer.class, (byte[]) obj);
         throw new RuntimeException("cannot convert null object");
     }
 
-    public static final long getLongValueFrom(Object obj) {
-        if (obj != null) {
-            if (obj instanceof Number) {
-                return ((Number) obj).longValue();
-            } else if (obj instanceof Character) {
-                return (long) ((Character) obj).charValue();
-            } else if (obj instanceof String) {
-                return Long.valueOf((String) obj);
-            } else if (obj instanceof Boolean) {
-                return (Boolean) obj ? 1l : 0l;
-            } else if (obj instanceof byte[]) {
-                return ByteArrayUtils.bytesToPrimitive(Long.class, (byte[]) obj);
-            }
-        }
-        throw new RuntimeException("cannot convert null object");
+    public static final long getLongValueFrom(@NonNull Object obj) {
+        if (obj instanceof Number)
+            return ((Number) obj).longValue();
+        if (obj instanceof Character)
+            return (long) ((Character) obj).charValue();
+        if (obj instanceof String)
+            return Long.valueOf((String) obj);
+        if (obj instanceof Boolean)
+            return (Boolean) obj ? 1l : 0l;
+        if (obj instanceof byte[])
+            return ByteArrayUtils.bytesToPrimitive(Long.class, (byte[]) obj);
+        throw new UnsupportedTypeException("Unsupported type: " + obj.getClass().getName());
     }
 
-    public static final float getFloatValueFrom(Object obj) {
-        if (obj != null) {
-            if (obj instanceof Number) {
-                return ((Number) obj).floatValue();
-            } else if (obj instanceof Character) {
-                return (float) ((Character) obj).charValue();
-            } else if (obj instanceof String) {
-                return Float.valueOf((String) obj);
-            } else if (obj instanceof Boolean) {
-                return (Boolean) obj ? 1f : 0f;
-            } else if (obj instanceof byte[]) {
-                return ByteArrayUtils.bytesToPrimitive(Float.class, (byte[]) obj);
-            }
-        }
-        throw new RuntimeException("cannot convert null object");
+    public static final float getFloatValueFrom(@NonNull Object obj) {
+        if (obj instanceof Number)
+            return ((Number) obj).floatValue();
+        if (obj instanceof Character)
+            return (float) ((Character) obj).charValue();
+        if (obj instanceof String)
+            return Float.valueOf((String) obj);
+        if (obj instanceof Boolean)
+            return (Boolean) obj ? 1f : 0f;
+        if (obj instanceof byte[])
+            return ByteArrayUtils.bytesToPrimitive(Float.class, (byte[]) obj);
+        throw new UnsupportedTypeException("Unsupported type: " + obj.getClass().getName());
     }
 
-    public static final double getDoubleValueFrom(Object obj) {
-        if (obj != null) {
-            if (obj instanceof Number) {
-                return ((Number) obj).doubleValue();
-            } else if (obj instanceof Character) {
-                return (double) ((Character) obj).charValue();
-            } else if (obj instanceof String) {
-                return Double.valueOf((String) obj);
-            } else if (obj instanceof Boolean) {
-                return (Boolean) obj ? 1d : 0d;
-            } else if (obj instanceof byte[]) {
-                return ByteArrayUtils.bytesToPrimitive(Double.class, (byte[]) obj);
-            }
-        }
-        throw new RuntimeException("cannot convert null object");
+    public static final double getDoubleValueFrom(@NonNull Object obj) {
+        if (obj instanceof Number)
+            return ((Number) obj).doubleValue();
+        if (obj instanceof Character)
+            return (double) ((Character) obj).charValue();
+        if (obj instanceof String)
+            return Double.valueOf((String) obj);
+        if (obj instanceof Boolean)
+            return (Boolean) obj ? 1d : 0d;
+        if (obj instanceof byte[])
+            return ByteArrayUtils.bytesToPrimitive(Double.class, (byte[]) obj);
+        throw new UnsupportedTypeException("Unsupported type: " + obj.getClass().getName());
     }
 
-    public static final short getShortValueFrom(Object obj) {
-        if (obj != null) {
-            if (obj instanceof Number) {
-                return ((Number) obj).shortValue();
-            } else if (obj instanceof Character) {
-                return (short) ((Character) obj).charValue();
-            } else if (obj instanceof String) {
-                return Short.valueOf((String) obj);
-            } else if (obj instanceof Boolean) {
-                return (short) ((Boolean) obj ? 1 : 0);
-            } else if (obj instanceof byte[]) {
-                return ByteArrayUtils.bytesToPrimitive(Short.class, (byte[]) obj);
-            }
-        }
-        throw new RuntimeException("cannot convert null object");
+    public static final short getShortValueFrom(@NonNull Object obj) {
+        if (obj instanceof Number)
+            return ((Number) obj).shortValue();
+        if (obj instanceof Character)
+            return (short) ((Character) obj).charValue();
+        if (obj instanceof String)
+            return Short.valueOf((String) obj);
+        if (obj instanceof Boolean)
+            return (short) ((Boolean) obj ? 1 : 0);
+        if (obj instanceof byte[])
+            return ByteArrayUtils.bytesToPrimitive(Short.class, (byte[]) obj);
+        throw new UnsupportedTypeException("Unsupported type: " + obj.getClass().getName());
     }
 
-    public static final byte getByteValueFrom(Object obj) {
-        if (obj != null) {
-            if (obj instanceof Number) {
-                return ((Number) obj).byteValue();
-            } else if (obj instanceof Character) {
-                return (byte) ((Character) obj).charValue();
-            } else if (obj instanceof String) {
-                return Byte.valueOf((String) obj);
-            } else if (obj instanceof Boolean) {
-                return (byte) ((Boolean) obj ? 1 : 0);
-            } else if (obj instanceof byte[]) {
-                return ByteArrayUtils.bytesToPrimitive(Byte.class, (byte[]) obj);
-            }
-        }
-        throw new RuntimeException("cannot convert null object");
+    public static final byte getByteValueFrom(@NonNull Object obj) {
+        if (obj instanceof Number)
+            return ((Number) obj).byteValue();
+        if (obj instanceof Character)
+            return (byte) ((Character) obj).charValue();
+        if (obj instanceof String)
+            return Byte.valueOf((String) obj);
+        if (obj instanceof Boolean)
+            return (byte) ((Boolean) obj ? 1 : 0);
+        if (obj instanceof byte[])
+            return ByteArrayUtils.bytesToPrimitive(Byte.class, (byte[]) obj);
+        throw new UnsupportedTypeException("Unsupported type: " + obj.getClass().getName());
     }
 
     /**
@@ -194,25 +173,23 @@ public class PrimitiveUtils {
      * @param obj
      * @return char represented by input obj
      */
-    public static final char getCharValueFrom(Object obj) {
-        if (obj != null) {
-            if (obj instanceof Number) {
-                return Character.toChars(((Number) obj).intValue())[0];
-            } else if (obj instanceof Character) {
-                return ((Character) obj).charValue();
-            } else if (obj instanceof String) {
-                if (((String) obj).length() > 0) {
-                    return ((String) obj).charAt(0);
-                } else {
-                    return '\0';
-                }
-            } else if (obj instanceof Boolean) {
-                return ((Boolean) obj ? '1' : '0');
-            } else if (obj instanceof byte[]) {
-                return ByteArrayUtils.bytesToPrimitive(Character.class, (byte[]) obj);
+    public static final char getCharValueFrom(@NonNull Object obj) {
+        if (obj instanceof Number)
+            return Character.toChars(((Number) obj).intValue())[0];
+        if (obj instanceof Character)
+            return ((Character) obj).charValue();
+        if (obj instanceof String) {
+            if (((String) obj).length() > 0) {
+                return ((String) obj).charAt(0);
+            } else {
+                return '\0';
             }
         }
-        throw new RuntimeException("cannot convert null object");
+        if (obj instanceof Boolean)
+            return ((Boolean) obj ? '1' : '0');
+        if (obj instanceof byte[])
+            return ByteArrayUtils.bytesToPrimitive(Character.class, (byte[]) obj);
+        throw new UnsupportedTypeException("Unsupported type: " + obj.getClass().getName());
     }
 
     /**
@@ -226,17 +203,16 @@ public class PrimitiveUtils {
      * @return
      */
     public static final boolean getBooleanValueFrom(Object obj) {
-        if (obj instanceof Number) {
+        if (obj instanceof Number)
             return ((Number) obj).doubleValue() != 0;
-        } else if (obj instanceof String) {
+        if (obj instanceof String)
             return Boolean.valueOf((String) obj);
-        } else if (obj instanceof Character) {
+        if (obj instanceof Character)
             return ((Character) obj).charValue() != '\0';
-        } else if (obj instanceof Boolean) {
+        if (obj instanceof Boolean)
             return ((Boolean) obj).booleanValue();
-        } else if (obj instanceof byte[]) {
+        if (obj instanceof byte[])
             return ByteArrayUtils.bytesToPrimitive(Boolean.class, (byte[]) obj);
-        }
         return obj != null;
     }
 }
