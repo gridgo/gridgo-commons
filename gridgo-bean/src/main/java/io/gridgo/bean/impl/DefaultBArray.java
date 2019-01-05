30c33be8a205ab73830637540fefcb011affa2d5
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-01-05 21:23:30 +0800

    [Bean] Prevent add/put null BElement for BContainers (BArray, BObject)

--- gridgo-bean/src/main/java/io/gridgo/bean/impl/DefaultBArray.java
+++ gridgo-bean/src/main/java/io/gridgo/bean/impl/DefaultBArray.java
@@ -1,12 +1,15 @@
 package io.gridgo.bean.impl;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import io.gridgo.bean.BArray;
 import io.gridgo.bean.BElement;
 import io.gridgo.bean.BFactory;
 import io.gridgo.bean.serialize.BSerializer;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @SuppressWarnings("unchecked")
@@ -27,5 +30,25 @@ class DefaultBArray extends ArrayList<BElement> implements BArray {
         StringBuilder sb = new StringBuilder();
         this.writeString(null, 0, sb);
         return sb.toString();
+    }
+
+    @Override
+    public boolean add(@NonNull BElement e) {
+        return super.add(e);
+    }
+
+    @Override
+    public void add(int index, @NonNull BElement element) {
+        super.add(index, element);
+    }
+
+    @Override
+    public boolean addAll(@NonNull Collection<? extends BElement> c) {
+        return super.addAll(c.stream().filter(ele -> ele != null).collect(Collectors.toList()));
+    }
+
+    @Override
+    public boolean addAll(int index, @NonNull Collection<? extends BElement> c) {
+        return super.addAll(index, c.stream().filter(ele -> ele != null).collect(Collectors.toList()));
     }
 }
